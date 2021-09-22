064261eb675fdedabf99d2240eaa67d65ba1caf3
Semyon Fedorov <semund.sigurdsson@gmail.com>
2021-09-22 11:50:08 +0300

    Work version of telegram bot

--- bot.py
+++ bot.py
@@ -1,51 +1,52 @@
 import telebot
-import question
+import question as quest
 from settings import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
+question = quest.Question()
+flag_question = None
 
 @bot.message_handler(func=lambda message: True)
 def quiz_game(message):
-    if "вопрос" in message.text.lower():
+    global flag_question
+    if message.text.lower() in("да", "+", "вопрос"):
+        question.reinit_question()
         bot.send_message(
             message.chat.id,
-            "Test question"
+            str(question)
         )
-    elif "игра" in message.text.lower():
-        bot.send_message(
-            message.chat.id,
-            "Let's play!"
-        )
+        flag_question= True
+    elif message.text.strip() in ("1", "2", "3", "4"):
+        if flag_question:
+            user_answer = int(message.text.strip())
+            if question.correct_answer == question.answers[user_answer - 1]:
+                msg = f"Совершенно верно, {message.from_user.first_name}! " \
+                          f"Правильный ответ - {question.correct_answer}\nЕще один?"
+            else:
+                msg = f"Неправильный ответ, {message.from_user.first_name}! " \
+                          f"Правильный ответ - {question.correct_answer}\nЕще один?"
+            bot.send_message(
+                message.chat.id,
+                msg
+            )
+            flag_question = None
+        else:
+            bot.reply_to(
+                message,
+                text=f"Я не задавал тебе вопрос, {message.from_user.first_name}\n"
+                     f"Я могу задать его тебе, просто напиши мне \"вопрос\""
+            )
     elif 'привет' in message.text.lower():
         bot.send_message(
             message.chat.id,
-            f"Приветствую, {message.from_user.first_name}!\nПоиграем или просто вопрос?"
+            f"Приветствую, {message.from_user.first_name}!\nЯ могу задать тебе вопрос, просто напиши мне \"вопрос\""
         )
     else:
         bot.reply_to(
             message,
-            text=f"Я пока мало что умею, {message.from_user.first_name}. Поиграем или просто вопрос?"
+            text=f"Я пока не понимаю тебя, {message.from_user.first_name}\n"
+                 f"Я могу только задать тебе вопрос, если ты напишешь мне \"вопрос\""
         )
 
 
 bot.polling()
-
-# quiz.main()
-
-
-
-# def get_user_answer():
-#     """
-#     Function waiting for input from the user until he enters the correct answer number in the range from 1 to 4
-#     """
-#     while True:
-#         try:
-#             user_answer = int(input('Введите Ваш ответ:  '))
-#             if not 1 <= user_answer <= 4:
-#                 raise ValueError
-#         except ValueError as exc:
-#             print('Выберите правильный ответ, введя число от 1 до 4!')
-#             continue
-#         else:
-#             break
-#     return user_answer
